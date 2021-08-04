1e4c95913d347288734f8fc8214e0b07a96e2aac
dlawlrb <54739299+dlawlrb@users.noreply.github.com>
2021-08-04 22:18:16 +0900

    Update BestAlbum.java

--- Daniel/Hash/BestAlbum.java
+++ Daniel/Hash/BestAlbum.java
@@ -1,29 +1,61 @@
-import java.util.HashMap;
+import java.util.*;
 
-public class Solution {
-    public String solution(String[] participant, String[] completion) {
-        HashMap<String, Integer> map = new HashMap<String, Integer>(100000);
-        for(int i=0; i<completion.length; i++)
+class Solution {
+    public int[] solution(String[] genres, int[] plays) {
+
+        HashMap<String, Integer[]> hash = new HashMap<String, Integer[]>(100);
+        for(int i=0; i<genres.length; i++)
         {
-            if(map.containsKey(completion[i]))
+            Integer[] play_list = hash.get(genres[i]);
+            if(play_list == null)
             {
-                var count = map.get(completion[i]);
-                map.put(completion[i], count+1);
+                hash.put(genres[i], new Integer[]{plays[i], -1,i,-1,plays[i]});
+                for (Map.Entry<String, Integer[]> entrySet : hash.entrySet()) {
+                    System.out.println(entrySet.getKey() + " : " + Arrays.toString(entrySet.getValue()));
+                }
             }
             else
             {
-                map.put(completion[i], 1);
+                if(play_list[0] < plays[i])
+                {
+                    play_list[4] = play_list[4] + plays[i];
+                    play_list[1] = play_list[0];
+                    play_list[3] = play_list[2];
+                    play_list[0] = plays[i];
+                    play_list[2] = i;
+
+                }
+                else if(play_list[1] < plays[i])
+                {
+                    play_list[4] = play_list[4] + plays[i];
+                    play_list[1] = plays[i];
+                    play_list[3] = i;
+                }
+                else
+                {
+                    play_list[4] = play_list[4] + plays[i];
+                }
+                hash.put(genres[i], play_list);
             }
         }
-        for(int i=0; i<participant.length; i++)
+        ArrayList<Integer> result = new ArrayList<>();
+
+        List<HashMap.Entry<String, Integer[]>> list_entries = new ArrayList<HashMap.Entry<String, Integer[]>>(hash.entrySet());
+        Collections.sort(list_entries, new Comparator<Map.Entry<String, Integer[]>>() {
+            // compare로 값을 비교
+            public int compare(Map.Entry<String, Integer[]> obj1, Map.Entry<String, Integer[]> obj2) {
+                // 내림 차순 정렬
+                return obj2.getValue()[4].compareTo(obj1.getValue()[4]);
+            }
+        });
+
+        for(Map.Entry<String, Integer[]> entry : list_entries)
         {
-            if(!map.containsKey(participant[i]))
-                return participant[i];
-            var count = map.get(participant[i]);
-            if(count == 0)
-                return participant[i];
-            map.put(participant[i], count-1);
+            Integer[] temp = entry.getValue();
+            result.add(temp[2]);
+            if(temp[3] != -1)
+                result.add(temp[3]);
         }
-        return "";
+        return result.stream().mapToInt(i->i).toArray();
     }
 }
